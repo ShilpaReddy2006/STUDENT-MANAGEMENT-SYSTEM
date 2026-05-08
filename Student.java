794ca617db45e7459cafbbf603706417dafa8194
ShilpaReddy2006 <shilpareddy24871@gmail.com>
2026-05-08 09:36:29 +0530

    Initial spring boot project upload
    Added Spring Boot backend project with REST API, MySQL integration, JPA, and layered architecture

---
+++ Student.java
@@ -0,0 +1,51 @@
+
+package com.sms.student_management_system.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String course;
+    private double fee;
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+}
