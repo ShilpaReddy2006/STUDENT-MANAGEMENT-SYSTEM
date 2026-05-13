36e087b1625d4e781ab7bf8743bcd02bb2ab7740
ShilpaReddy2006 <shilpareddy24871@gmail.com>
2026-05-13 10:06:23 +0530

    Update and rename StudentController.java to src/main/java/com/sms/student_management_system/controller/StudentController.java

---
+++ src/main/java/com/sms/student_management_system/controller/StudentController.java
@@ -0,0 +1,33 @@
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public Student saveStudent(@RequestBody Student student) {
+        return studentService.saveStudent(student);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable int id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable int id, @RequestBody Student student) {
+        return studentService.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable int id) {
+        studentService.deleteStudent(id);
+        return "Student deleted successfully";
+    }
+}
